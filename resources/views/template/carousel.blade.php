9ff78f0d22d8baca94fc476482241deaef7efda1
kukuhpradypta <kukuhpradypta@gmail.com>
2021-09-24 15:40:02 +0700

    iframe

--- resources/views/template/carousel.blade.php
+++ resources/views/template/carousel.blade.php
@@ -1,23 +1,35 @@
 
-    <div id="carouselExampleControls" class="carousel slide counts section-bg" data-bs-ride="carousel">
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <div class="container">
-              <div class="row counters kebawah">
-                <div class="col-md-4">
+  <div id="carouselExampleIndicators " class="carousel slide carosel" data-bs-ride="carousel">
+  <div class="carousel-inner pt-4 ">
+    <div class="carousel-item active">
+            <div class=" container text-white  carosel mb-5">
+              <div class="row counters">
+                <div class="col-md-4 judul">
                   <h2 style="color: white">Kegiatan Assesmen ANBK 2021</h2>
                 </div>
                 <div class="col-md-4">
-                  <p style="font-weight: 100" class="mt-2">kegiatan ini di laksanakan pada tanggal 20 - 23 September 2021 di mulai pukul 07.40 sd 12.00 wib dengan lancar</p>
+                  <p style="font-weight: 100" class="mt-2 isi">kegiatan ini di laksanakan pada tanggal 20 - 23 September 2021 di mulai pukul 07.40 sd 12.00 wib dengan lancar</p>
                 </div>
-                <div class="col-md-4 text-center">
-                  <a href="#" style="background-color: #e39b0d; border-radius: 50px" class="btn text-center fw-bold text-white pe-5 ps-5 mt-4">Selengkapnya</a>
+                <div class="col-md-4 text-center tombol">
+                  <a href="#" style="background-color: #e39b0d; border-radius: 50px" class="btn  text-center fw-bold text-white pe-5 ps-5 mt-4">Selengkapnya</a>
                 </div>
               </div>
             </div>
           </div>
-
-          <!-- Slide 2 -->
+              <div class="carousel-indicators skipback" >
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+ {{-- <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button> --}}
+          {{-- <!-- Slide 2 -->
           <div class="carousel-item" id="counts">
             <div class="container">
               <div class="row counters kebawah">
@@ -55,14 +67,7 @@
                 </div>
               </div>
             </div>
-          </div>
-          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-          </button>
-          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-          </button>
+          </div> --}}
+  </div>
         </div>
     </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -53,7 +53,7 @@
       <div class="container position-relative" data-aos="zoom-in" data-aos-delay="100">
         <h1>Learning Today,<br />Leading Tomorrow</h1>
         <h2>We are team of talented designers making websites with Bootstrap</h2>
-        <a href="courses.html" class="btn-get-started">Get Started</a>
+        <a href="https://portal.smktarunabhakti.net/login/index.php" target="_blank" class="btn-get-started">Get Started</a>
       </div>
     </section>
     <!-- End Hero -->
