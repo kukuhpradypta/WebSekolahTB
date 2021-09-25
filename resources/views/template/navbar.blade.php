66d201134bcc6cce21e3e9dafee6a13817409810
kukuhpradypta <kukuhpradypta@gmail.com>
2021-09-25 15:21:49 +0700

    benerin navbar

--- resources/views/template/navbar.blade.php
+++ resources/views/template/navbar.blade.php
@@ -47,15 +47,15 @@
         <!-- Uncomment below if you prefer to use an image logo -->
         <a href="index.html" class="logo me-auto"><img src="{{asset('template/assets/img/logo.png')}}" alt="" class="img-fluid"></a>
 
-        <nav id="navbar active" class="navbar order-last order-lg-0" style="margin-left: auto">
+        <nav id="navbar active" class="navbar order-last order-lg-0 {{request()->is('/') || request()->is('profile') || request()->is('kurikulum') || request()->is('hubin') || request()->is('sarpras') || request()->is('kesiswaan') || request()->is('kontak') ? 'show':''}}" style="margin-left: auto">
           <ul>
-            <li><a class="active" href="/">Home</a></li>
-            <li><a href="/profile">Profile</a></li>
-            <li><a href="/kurikulum">Kurikulum</a></li>
-            <li><a href="trainers.html">Hubungan Industri</a></li>
-            <li><a href="events.html">Sarana Pra Sarana</a></li>
-            <li><a href="pricing.html">Kesiswaan</a></li>
-            <li><a href="contact.html">Kontak Kami</a></li>
+            <li><a class="{{request()->is('/*') ? 'active':''}}" href="/">Home</a></li>
+            <li><a class="{{request()->is('profile*') ? 'active':''}}" href="/profile">Profile</a></li>
+            <li><a class="{{request()->is('kurikulum*') ? 'active':''}}" href="/kurikulum">Kurikulum</a></li>
+            <li><a class="{{request()->is('hubin*') ? 'active':''}}" href="trainers.html">Hubungan Industri</a></li>
+            <li><a class="{{request()->is('sarpras*') ? 'active':''}}" href="events.html">Sarana Pra Sarana</a></li>
+            <li><a class="{{request()->is('kesiswaan*') ? 'active':''}}" href="pricing.html">Kesiswaan</a></li>
+            <li><a class="{{request()->is('kontak*') ? 'active':''}}" href="contact.html">Kontak Kami</a></li>
           </ul>
           <i class="bi bi-list mobile-nav-toggle"></i>
         </nav>
